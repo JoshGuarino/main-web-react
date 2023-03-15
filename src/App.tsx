5c97b3e2f91893829de50f52b930256604689aea
josh-guarino <josh.guarino@eagleview.com>
2023-03-15 18:59:22 -0400

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Nav from './components/nav'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
+    // <Nav></Nav>
   )
 }
 
